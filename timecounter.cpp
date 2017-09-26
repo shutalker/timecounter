5772235c6803b06ec40822a8ae49104da24c381f
= <shatunov.alexey@mail.ru>
2017-09-26 20:02:44 +0300

    timecounter rev.2: added setpriority() call to constructor; removed rdtsc_*() methods since cpu ticks calculated from time and cpu frequency

--- timecounter.cpp
+++ timecounter.cpp
@@ -1,36 +1,5 @@
 #include "timecounter.h"
 
-/* ONLY FOR TESTING RDTSC VS GETRUSAGE + CPU_FREQ */
-
-void TimeCounter::rdtsc_start()
-{
-    unsigned int  tsc_high;   // high 32 bit of TSC register
-    unsigned int  tsc_low;    // low 32 bit of TSC register
-
-    asm volatile ("rdtscp"
-        : "=a" (tsc_low), "=d" (tsc_high)
-        :
-        : "%ecx");
-
-    tsc_start = ( unsigned long long ) tsc_low |
-                ( ( ( unsigned long long ) tsc_high ) << 32 );
-}
-
-void TimeCounter::rdtsc_end()
-{
-    unsigned int  tsc_high;   // high 32 bit of TSC register
-    unsigned int  tsc_low;    // low 32 bit of TSC register
-
-    asm volatile ("rdtscp"
-        : "=a" (tsc_low), "=d" (tsc_high)
-        :
-        : "%ecx");
-
-    tsc_end = ( unsigned long long ) tsc_low |
-              ( ( ( unsigned long long ) tsc_high ) << 32 );
-}
-
-/* END TESTING BLOCK */
 
 void TimeCounter::get_cpu_freq()
 {
@@ -57,8 +26,10 @@ void TimeCounter::get_cpu_freq()
     }
 }
 
-TimeCounter::TimeCounter( const string &label_name ):
+TimeCounter::TimeCounter( const string &label_name,
+                          TimeCounterNotify *notifier ):
     label( label_name ),
+    notify( notifier ),
     cpu_freq( 0 ),
     error_flag( 0 )
 {
@@ -77,14 +48,21 @@ TimeCounter::TimeCounter( const string &label_name ):
         return;
     }
 
+    // set process priority to 20    
+    if( setpriority( PRIO_PROCESS, 0, 20 ) == -1 )
+    {
+        perror("Setpriority: ");
+        error_flag = 1;
+        return;
+    }
+
     // start timer
-//    rdtsc_start();
     getrusage( RUSAGE_SELF, &timer_start );
 }
 
 TimeCounter::~TimeCounter()
 {
-//    rdtsc_end();
+    // stop timer
     getrusage( RUSAGE_SELF, &timer_end );
 
     if( error_flag )
@@ -93,7 +71,6 @@ TimeCounter::~TimeCounter()
         return;
     }
 
-    // get current cpu frequency
     get_cpu_freq();
 
     // get time ( process user mode time + process kernel mode time )
@@ -110,12 +87,11 @@ TimeCounter::~TimeCounter()
                     timer_start.ru_stime.tv_usec );
 
     // get ticks amount
-//    auto ticks = tsc_end - tsc_start;
-    auto ticks = timer_diff * ( unsigned long long ) cpu_freq;
-    auto diff_in_msecs =  ( double ) timer_diff / ( double ) 1000;
+    unsigned long long ticks = timer_diff * ( unsigned long long ) cpu_freq;
+    double  diff_in_msecs =  ( double ) timer_diff / ( double ) 1000;
 
-//    if( notify )
-//        notify->report( diff_in_msecs, ticks, &label );
+    if( notify )
+        notify->report( diff_in_msecs, ticks, &label );
 
     cout << label <<  ": " << diff_in_msecs << " ms; "
          << ticks << " ticks" << endl;
--- timecounter.h
+++ timecounter.h
@@ -7,7 +7,6 @@
 #include <sys/sysctl.h>    // sysctl(), sysctlnametomib()
 #include <sys/cpuset.h>    // cpuset_setaffinity(), CPUSET_T_INITIALIZER()
 #include <sys/resource.h>  // getrusage()
-#include <unistd.h>        // exit()
 
 using namespace std;
 
@@ -15,18 +14,10 @@ using namespace std;
 class TimeCounter
 {
     const string  label;
-    // TimeCounterNotify *notify;
+     TimeCounterNotify *notify;
 
     struct rusage timer_start;
     struct rusage timer_end;
-
-    /* ONLY FOR TESTING RDTSC VS GETRUSAGE + CPU_FREQ */
-
-    // values of TSC register for cpu cycles counting
-    unsigned long long tsc_start;
-    unsigned long long tsc_end;
-
-    /* END TESTING BLOCK */
 
     int cpu_freq;
 
@@ -34,16 +25,8 @@ class TimeCounter
     int error_flag;
 
 public:
-    TimeCounter( const string &label_name/*,  TimeCounterNotify *notify */);
+    TimeCounter( const string &label_name,  TimeCounterNotify *notifier);
     ~TimeCounter();
-
-    /* ONLY FOR TESTING RDTSC VS GETRUSAGE + CPU_FREQ */
-
-    // asm realization of RDTSC
-    void rdtsc_start();
-    void rdtsc_end();
-
-    /* END TESTING BLOCK */
 
     // current cpu frequency getter
     void get_cpu_freq();
